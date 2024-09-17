41ee5b0eb2e85c8994804a15327718ad8ac0de62
swizzlestix09 <angely.rodriguez@gmail.com>
2024-09-17 16:23:31 -0400

    signup button leads to /signup

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,19 +1,35 @@
+"use client"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+type UserInfo = {
+  name: string,
+  password: string
+}
 
 export default function Page() {
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    name: '',
+    password: ''
+  })
+  const router = useRouter();
+
+  const handleSignUpClick = () => router.push('/signup')
+
   return (
 
     <div className="w-1/3 mx-auto justify-center items-center">
-
       <form className="flex-col align-center">
         <p>Sign-in</p>
         <Input placeholder="Email" />
         <Input placeholder="Password" />
         <Button type="submit">Sign-In</Button>
-        <Button type="submit">Sign-Up</Button>
+        <Button type="button" onClick={handleSignUpClick}>Sign-Up</Button>
       </form>
     </div>
 
   )
 }
+
