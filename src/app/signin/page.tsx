72e97f24b1fc7b9581ba0915a12cf6929e39d3e3
swizzlestix09 <angely.rodriguez@gmail.com>
2024-09-17 16:38:24 -0400

    working on sign in

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,26 +5,49 @@ import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 type UserInfo = {
-  name: string,
+  email: string,
   password: string
 }
 
 export default function Page() {
   const [userInfo, setUserInfo] = useState<UserInfo>({
-    name: '',
+    email: '',
     password: ''
   })
   const router = useRouter();
+
+  const handleUserInfoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    console.log(name, value)
+    setUserInfo(prevInfo => ({ ...prevInfo, [name]: value }))
+
+  }
+
+  const handleUserInfoSubmit = () => {
+    //send info to database
+    //if error update UI
+    //if no errors progress to next page
+  }
 
   const handleSignUpClick = () => router.push('/signup')
 
   return (
 
-    <div className="w-1/3 mx-auto justify-center items-center">
+    <div className="w-1/3 mx-auto justify-center items-center" onSubmit={(e) => e.preventDefault()}>
       <form className="flex-col align-center">
         <p>Sign-in</p>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
+        <Input
+          name='email'
+          placeholder="Email"
+          value={userInfo.email}
+          onChange={handleUserInfoChange}
+        />
+        <Input
+          name='password'
+          placeholder="Password"
+          type="password"
+          value={userInfo.password}
+          onChange={handleUserInfoChange} />
         <Button type="submit">Sign-In</Button>
         <Button type="button" onClick={handleSignUpClick}>Sign-Up</Button>
       </form>
