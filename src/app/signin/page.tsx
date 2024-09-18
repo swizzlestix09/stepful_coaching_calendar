62222829cc916306710f3c7a8e4611ef7f8cd196
swizzlestix09 <angely.rodriguez@gmail.com>
2024-09-17 21:28:14 -0400

    notes

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -58,3 +58,26 @@ export default function Page() {
   )
 }
 
+
+
+/*
+ * get information on how to make a function that posts information to the database
+ * double checking the user information  if successful we have to check who is a coach and who is a student
+ * otherwise they have to be redirected to the homepage (/signin)
+ *
+ * Once this is complete can move on to calendar
+ *  1) allow a coach to create bookings
+ *    - pick a day
+ *     -how do i enable a coach to pick a day?
+ * Create a calendar with shadcn , use state for date
+ *   how to select a time? Input?
+ *   also what is the format of the time
+ *   how to store time in postgrs sql
+ *     do not book past (before today) time
+ *   once  time + day is selected enable save
+ *     is booked in slots needs to be updated
+ *      booking needs slot id and student id updated
+ *
+ *
+ * - 2 hour time slot
+ *
